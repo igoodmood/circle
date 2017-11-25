37ad10d61171b7c49feb4148415584bcca26dd68
igoodmood <vladimirscherbina60@gmail.com>
2017-11-25 11:59:17 +0200

    Lol

--- com/company/Circle.java
+++ com/company/Circle.java
@@ -14,7 +14,7 @@ public class Circle extends JFrame {
     private JButton sort;
     static Integer[] array = new Integer[15];
     public Circle() {
-        super("Test1");
+        super("Squarerer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         squares = new Squares();
         this.setLayout(new BorderLayout());
